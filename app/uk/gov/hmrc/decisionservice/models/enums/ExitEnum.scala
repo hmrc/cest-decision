d03a43c3e64c8ba6e3c671e818442ea2e42123f3
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2019-07-19 15:28:01 +0100

    removed unknown from exit enum

--- app/uk/gov/hmrc/decisionservice/models/enums/ExitEnum.scala
+++ app/uk/gov/hmrc/decisionservice/models/enums/ExitEnum.scala
@@ -24,6 +24,5 @@ object ExitEnum extends Enumeration with EnumFormats{
   val CONTINUE: ExitEnum.Value = Value("CONTINUE")
   val NOT_VALID_USE_CASE: ExitEnum.Value = Value("NotValidUseCase")
   val INSIDE_IR35: ExitEnum.Value = Value("INIR35")
-  val UNKNOWN: ExitEnum.Value = Value("Unknown")
   implicit val format: Format[ExitEnum.Value] = enumFormat(ExitEnum)
 }
