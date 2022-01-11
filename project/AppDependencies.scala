6d1d05781cc060f6d7d9545591a36b47728375aa
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-01-11 12:07:36 +0000

    DL-7050 dependency and plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "http-caching-client"       % "9.5.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.16.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.18.0",
     "com.typesafe.play" %% "play-json-joda"            % "2.9.2",
     ws,
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.1" cross CrossVersion.full),
@@ -24,13 +24,13 @@ object AppDependencies {
   object Test {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
-        "org.pegdown"            % "pegdown"                      % pegdownVersion           % scope,
+        "org.pegdown"            %  "pegdown"                     % pegdownVersion           % scope,
         "com.typesafe.play"      %% "play-test"                   % current                  % scope,
-        "org.mockito"            % "mockito-core"                 % "4.0.0"                  % scope,
-        "org.mockito"            %% "mockito-scala-scalatest"     % "1.16.46"                % scope,
+        "org.mockito"            %  "mockito-core"                % "4.2.0"                  % scope,
+        "org.mockito"            %% "mockito-scala-scalatest"     % "1.16.49"                % scope,
         "org.scalatestplus.play" %% "scalatestplus-play"          % scalaTestPlayPlusVersion % scope,
         "org.scalamock"          %% "scalamock-scalatest-support" % "3.6.0"                  % scope,
-        "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % "5.16.0"                 % scope
+        "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % "5.18.0"                 % scope
       )
     }.test
   }
@@ -41,11 +41,11 @@ object AppDependencies {
       override lazy val scope: String = "it"
 
       override lazy val test = Seq(
-        "org.pegdown"            % "pegdown"                 % pegdownVersion           % scope,
+        "org.pegdown"            %  "pegdown"                % pegdownVersion           % scope,
         "com.typesafe.play"      %% "play-test"              % current                  % scope,
         "org.scalatestplus.play" %% "scalatestplus-play"     % scalaTestPlayPlusVersion % scope,
-        "com.github.tomakehurst" % "wiremock-standalone"     % "2.27.2"                 % scope,
-        "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.16.0"                 % scope
+        "com.github.tomakehurst" %  "wiremock-standalone"    % "2.27.2"                 % scope,
+        "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.18.0"                 % scope
       )
     }.test
   }
